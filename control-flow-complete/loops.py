adcff3decdd637608ef6b1ad4839e50ce9575d41
Michael Adair <mjadair91@gmail.com>
2020-04-01 12:45:50 +0100

    more control flow added

--- control-flow-complete/loops.py
+++ control-flow-complete/loops.py
@@ -8,79 +8,69 @@
 
 # ? Write a for loop that logs the numbers 0 - 10
 
-for (let i = 0; i <= 10; i++) {
-  console.log(i)
-}
+for i in range(11):
+  print(i)
+
 
 # ? Write a for loop that console logs the string 'Hello World' 10 times
 
-for (let i = 0; i < 10; i++) {
-  console.log('Hello World')  
-}
+for i in range(10):
+    print('Hello World')
 
 # ? Write a for loop that console logs the numbers 50 to 88
 
-for (let i = 50; i <= 88; i++) {
-  console.log(i)
-}
+for i in range(89):
+    if i < 50:
+        continue
+    print(i)
 
-# ? BONUS - Declare a "let" with the label "result" and the value empty string.
+# ? BONUS - Declare a variable called "result" and the value of an empty string.
 
-# ? use a for loop and string concatenation to built the string '123456789' and log it to the console
+# ? use a for loop and string concatenation to built the string '123456789' and print to the console
 
-let result = ''
+result = ''
 
-for (let i = 1; i <= 9; i++) {
-  result = result + i
-}
+for i in range(10):
+    result = result + str(i)
 
-console.log(result)
+print(result)
 
 
 # *  ----- WHILE LOOPS ------ *
-# *  💻 Remember "fn" key + "f8" to run  your code
+# ! Remember, run`python loops.py` to run the file
 
 # * 🦉 Practice
-
-
-
-
-
-
-
-
 
 
 # ! ⚠️Remember to comment out your practice code before attempting below, "cmd" + "/"
 
 # ? Write a while loop that logs the numbers 0 - 10
 
-let num = 0
+num = 0
 
-while (num <= 10) {
-  console.log(num)
-  num++
-}
+while num <= 10:
+  print(num)
+  num += 1
+
 
 # ? Write a while that console logs the string 'Hello World' 5 times
 
-let current = 0
+current = 0
 
-while (current < 5) {
-  console.log('Hello World')
-  current++
-}
+while (current < 5):
+  print('Hello World')
+  current += 1
 
 
 # ? BONUS - Write a while loop that will check if a random number is above or below 0.5, If it is the loop should print 'still running!' and the random number. When it has finished, it should log 'Stopped', and the randomNumber is stopped with. How many times will this log print?
 
-let randomNumber = Math.random()
+import random
 
-while (randomNumber > 0.5) {
-  console.log('still running!')
-  console.log(randomNumber)
-  randomNumber = Math.random()
-}
+randomNumber = (random.random())
 
-console.log('Stopped')
-console.log(randomNumber)
+while randomNumber > 0.5:
+  print('still running!')
+  print(randomNumber)
+  randomNumber = (random.random())
+print('Stopped')
+print(randomNumber)
