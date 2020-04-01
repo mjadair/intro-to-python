ee5d653ba3b120eeac9531c6df46e07dfebe5576
Michael Adair <mjadair91@gmail.com>
2020-04-01 17:52:11 +0100

    checked

--- intro-to-python/numbers.py
+++ intro-to-python/numbers.py
@@ -1,63 +1,36 @@
-// *  ----- NUMBERS 🧮 ------
-// *  💻 Remember "fn" key + "f8" to run  your code
+# *  ----- NUMBERS 🧮 ------
 
-// * 🦉 Practice
-
+# Navigate to intro-to-python-complete folder and type python numbers.py to run
 
 
+# * 🦉 Practice
 
 
+# ! ⚠️Remember to comment out your practice code before attempting below, "cmd" + "/"
+
+# ? Define a variable with the label "num" and assign the value 10
 
 
-
-// ! ⚠️Remember to comment out your practice code before attempting below, "cmd" + "/"
-
-// ? Define a "const" with the label "num" and assign the value 10 
+# ? Define a variable "result", assign its value to our "num" + 50, and print it --> 60
 
 
-
-// ? Define a variable "result" using the "let" keyword, assign its value to our "num" + 50, and log it --> 60
-
+# ? reassign "result", to be the value of itself divided by 2, print it out --> 30
 
 
-// ? reassign "result", to be the value of itself divided by 2, log it out --> 30
+# ? reassign "result" again, to be the value of itself minus 9 --> 21
 
 
-
-// ? reassign "result" again, to be the value of itself minus 9 --> 21
-
+# ? reassign "result" again, to be value of itself divided by 7 --> 3
 
 
-// ? reassign "result" again, to be value of itself divided by 7 --> 3
+# ? Print the float 3.3 to the nearest number
 
 
-// *  ----- MATH OBJECT 🧮 ------
-// *  💻 Remember "fn" key + "f8" to run  your code
-
-// * 🦉 Practice
+# ? Print the float 10.8 rounded to 10
 
 
+# ? Print the float 4.2 round to 5
 
 
+# ? Print a random number between 1 and 10
 
-
-
-
-// ! ⚠️Remember to comment out your practice code before attempting below, "cmd" + "/"
-
-// ? Using the in built Math Object, try the following:
-
-
-// ? Log the float 3.3 to the nearest number
-
-
-
-// ? Log the float 10.8 rounded to 10
-
-
-
-// ? Log the float 4.2 round to 5
-
-
-
-// ? Log a random number between 1 and 10
