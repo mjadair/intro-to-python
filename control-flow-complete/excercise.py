15c6322fafdf662aeaa828a0ced643a73d6004ac
Michael Adair <mjadair91@gmail.com>
2020-04-01 12:52:44 +0100

    added fizzbuzz

--- control-flow-complete/excercise.py
+++ control-flow-complete/excercise.py
@@ -3,3 +3,16 @@
 # ? Any number that is divisible by 3 should log "Fizz" instead of the number.
 # ? Any number that is divisible by 5 should log "Buzz" instead of the number.
 # ? Any number that is divisible by 3 and 5 should log "FizzBuzz" instead of the number.
+
+
+for i in range(102):
+    if i == 0:
+        continue
+    if i % 3 == 0 and i % 5 == 0:
+        print('Fizzbuzz')
+    elif i % 3 == 0:
+        print('Fizz')
+    elif i % 5 == 0:
+        print('Buzz')
+    else:
+        print(i)
