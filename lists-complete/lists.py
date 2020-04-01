1530a32fee0c9d6f35069293f098b903e8ef6623
Michael Adair <mjadair91@gmail.com>
2020-04-01 17:09:41 +0100

    more lists

--- lists-complete/lists.py
+++ lists-complete/lists.py
@@ -1,39 +1,32 @@
 # *  ----- ARRAYS  ------ *
-# *  💻 Remember "fn" key + "f8" to run  your code
+# ! Navigate to the directory and type `python lists.py` to run the file
 
 # * 🦉 Practice
-
-
-
-
-
-
-
 
 # ! ⚠️Comment out your practice code before attempting below, "cmd" + "/"
 
 
-# ? Define a "const" students that is an array. Add the name of four of your classmates as strings.
+# ? Define a variable called students that is a list. Add the name of four of your classmates as strings.
 
-const students = ['Jack', 'Charlotte', 'Abi', 'Guy']
+students = ['Nick', 'Guy', 'Michael', 'Jonny']
 
 # ? Access the first name in the array and log it to the console.
 
-console.log(students[0])
+print(students[0])
 
 # ? Access the third name in the array and log it to the console.
 
-console.log(students[2])
+print(students[2])
 
 # ? Reassign the last item in the array to be a different name and log it
 
-students[3] = 'Mike'
-console.log(students[3])
+students[3] = 'Little Fiddy Finkerstein'
+print(students[3])
 
 # ? Log the length of the array 
 
-console.log(students.length)
+print(len(students))
 
 # ? Use the length property to log the final name in the array
 
-console.log(students[students.length])
+print(students[len(students) - 1])
