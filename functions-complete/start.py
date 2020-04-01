e7454cdbf09dafce3fd8dfcbe150a343536d9b15
Michael Adair <mjadair91@gmail.com>
2020-04-01 17:49:40 +0100

    start

---
+++ functions-complete/start.py
@@ -0,0 +1,9 @@
+# ! Navigate to the directory and type `python start.py` to run the file
+
+print('Hello World')
+
+# * This session we will be learning about functions in Python 👨‍🏫👩‍🏫
+
+# TODO: Definings functions
+# TODO: Arguments and parameters
+# TODO: Scope
